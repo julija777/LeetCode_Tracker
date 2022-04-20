1d83112c451881cdc3103b6b2ed9ac953d2cf7d7
Julija Stepanova <32721917+julija777@users.noreply.github.com>
2022-04-20 13:20:36 +0100

    Time: 54 ms (98.59%), Space: 42.6 MB (79.38%) - LeetHub

---
+++ 19-remove-nth-node-from-end-of-list/19-remove-nth-node-from-end-of-list.js
@@ -0,0 +1,24 @@
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val, next) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.next = (next===undefined ? null : next)
+ * }
+ */
+/**
+ * @param {ListNode} head
+ * @param {number} n
+ * @return {ListNode}
+ */
+ 
+
+//googled Linked list, Floyd's fast and slow algorithm
+
+var removeNthFromEnd = function(head, n) {
+    let fast = head, slow = head
+    for (let i = 0; i < n; i++) fast = fast.next
+    if (!fast) return head.next
+    while (fast.next) fast = fast.next, slow = slow.next
+    slow.next = slow.next.next
+    return head
+};
