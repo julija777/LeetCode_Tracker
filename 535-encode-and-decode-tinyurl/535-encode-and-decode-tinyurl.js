99bf1a092e3c48cd8236a71fcbee6efe6b6936c9
Julija Stepanova <32721917+julija777@users.noreply.github.com>
2022-04-23 19:07:57 +0100

    Time: 71 ms (83.81%), Space: 43.6 MB (84.76%) - LeetHub

---
+++ 535-encode-and-decode-tinyurl/535-encode-and-decode-tinyurl.js
@@ -0,0 +1,25 @@
+/**
+ * Encodes a URL to a shortened URL.
+ *
+ * @param {string} longUrl
+ * @return {string}
+ */
+var encode = function(longUrl) {
+    return encodeURI(longUrl)
+};
+
+/**
+ * Decodes a shortened URL to its original URL.
+ *
+ * @param {string} shortUrl
+ * @return {string}
+ */
+var decode = function(shortUrl) {
+    return decodeURI(shortUrl)
+    
+};
+
+/**
+ * Your functions will be called as such:
+ * decode(encode(url));
+ */
