c8213b73a06f1098925eb7e14e6860f2e906e893
Julija Stepanova <32721917+julija777@users.noreply.github.com>
2022-06-14 17:19:07 +0100

    Time: 72 ms (90.23%), Space: 42.8 MB (39.04%) - LeetHub

---
+++ 1-two-sum/1-two-sum.js
@@ -0,0 +1,15 @@
+/**
+ * @param {number[]} nums
+ * @param {number} target
+ * @return {number[]}
+ */
+var twoSum = function(nums, target) {
+    let map = {};
+    for (let i = 0; i < nums.length; i++) {
+        let diff = target - nums[i];
+        if (map[diff] !== undefined) {
+            return [map[diff], i];
+        }
+        map[nums[i]] = i;
+    }
+}
